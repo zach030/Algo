47d659875ea25944773861a88bdb13b88aac5945
Zach <zachchou016@gmail.com>
2021-06-10 15:35:10 +0800

    [add] array queue

---
+++ queue/queue.go
@@ -0,0 +1,62 @@
+package queue
+
+import "log"
+
+type Queue interface {
+	EnQueue(string2 string) bool
+	DeQueue() string
+}
+
+// 顺序队列
+type SeqQueue struct {
+	items []string
+	size  int
+	head  int
+	tail  int
+}
+
+func NewSeqQueue(size int) *SeqQueue {
+	return &SeqQueue{
+		items: make([]string, size),
+		size:  size,
+		head:  0,
+		tail:  0,
+	}
+}
+
+func (s *SeqQueue) EnQueue(str string) bool {
+	if s.tail == s.size {
+		if s.head == 0 {
+			log.Println("queue is full")
+			return false
+		}
+		for i := s.head; i < s.tail; i++ {
+			s.items[i-s.head] = s.items[i]
+		}
+		s.tail -= s.head
+		s.head = 0
+	}
+	s.items[s.tail] = str
+	s.tail++
+	return true
+}
+
+func (s *SeqQueue) DeQueue() string {
+	if s.head == s.tail {
+		log.Println("queue is empty")
+		return ""
+	}
+	ele := s.items[s.head]
+	s.head++
+	return ele
+}
+
+// 链式队列
+type LinkedQueue struct {
+}
+
+
+// 循环队列
+type RingQueue struct {
+
+}
