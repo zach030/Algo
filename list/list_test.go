64f5c44377a8a283f847bc45b5bd380ab91be8cd
Zach <zachchou016@gmail.com>
2021-06-28 09:33:25 +0800

    [add] reverse list

--- list/list_test.go
+++ list/list_test.go
@@ -1,6 +1,7 @@
 package list
 
 import (
+	"fmt"
 	"testing"
 )
 
@@ -20,4 +21,39 @@ func TestList_Add(t *testing.T) {
 	l.DeleteElement(3)
 
 	l.Print()
+}
+
+type ListNode struct {
+	Val int
+	Next *ListNode
+}
+
+func reverseList(head *ListNode) *ListNode {
+	var prev *ListNode
+	curr := head
+	for curr!=nil{
+		tmp := curr.Next
+		curr.Next = prev
+		prev = curr
+		curr = tmp
+	}
+	return prev
+}
+
+func TestReverseList(t *testing.T){
+	h := &ListNode{
+		Val:  1,
+		Next: &ListNode{
+			Val:  2,
+			Next: &ListNode{
+				Val:  3,
+				Next: nil,
+			},
+		},
+	}
+	res := reverseList(h)
+	for res!=nil{
+		fmt.Println(res.Val)
+		res = res.Next
+	}
 }
