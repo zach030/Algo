a652d155afed524477391f293a67ef7cd204c1f4
zach030 <946778530@qq.com>
2021-07-13 00:45:33 +0800

    [add] traverse tree

--- tree/tree.go
+++ tree/tree.go
@@ -1,26 +1,28 @@
 package tree
 
-type TreeNode struct{
-	Val int
-	Left *TreeNode
+type TreeNode struct {
+	Val   int
+	Left  *TreeNode
 	Right *TreeNode
 }
 
 // 层序遍历
 func levelOrder(root *TreeNode) [][]int {
-    ret := make([][]int,0)
+	ret := make([][]int, 0)
 	// queue用来保存子层级的节点
-	queue := make([]*TreeNode,0)
-	queue = append(queue, root)
-	for len(queue)!=0{
+	queue := make([]*TreeNode, 0)
+	if root != nil {
+		queue = append(queue, root)
+	}
+	for len(queue) != 0 {
 		size := len(queue)
-		row := make([]int,0)
-		for i:=0;i<size;i++{
+		row := make([]int, 0)
+		for i := 0; i < size; i++ {
 			// 取队列头
 			node := queue[0]
 			row = append(row, node.Val)
 			queue = queue[1:]
-			if node.Left!=nil{
+			if node.Left != nil {
 				queue = append(queue, node.Left)
 			}
 			if node.Right != nil {
@@ -30,4 +32,85 @@ func levelOrder(root *TreeNode) [][]int {
 		ret = append(ret, row)
 	}
 	return ret
+}
+
+// 锯齿形状层序遍历
+func zigzagLevelOrder(root *TreeNode) [][]int {
+	ret := make([][]int, 0)
+	// queue用来保存子层级的节点
+	queue := make([]*TreeNode, 0)
+	if root != nil {
+		queue = append(queue, root)
+	}
+	reverse := false
+	for len(queue) != 0 {
+		size := len(queue)
+		row := make([]int, 0)
+		for i := 0; i < size; i++ {
+			// 取队列头
+			node := queue[0]
+			row = append(row, node.Val)
+			queue = queue[1:]
+			if node.Left != nil {
+				queue = append(queue, node.Left)
+			}
+			if node.Right != nil {
+				queue = append(queue, node.Right)
+			}
+		}
+		if reverse {
+			row = reverseSlice(row)
+			ret = append(ret, row)
+			reverse = false
+		} else {
+			reverse = true
+			ret = append(ret, row)
+		}
+	}
+	return ret
+}
+
+func reverseSlice(s []int) []int {
+	size := len(s)
+	ret := make([]int, size)
+	for i, num := range s {
+		ret[size-1-i] = num
+	}
+	return ret
+}
+
+// 最近公共祖先
+func lowestCommonAncestor(root, p, q *TreeNode) *TreeNode {
+	//todo
+	return root
+}
+
+var max = 0
+
+// 二叉树直径
+func diameterOfBinaryTree(root *TreeNode) int {
+	if root != nil {
+		_ = calcHeight(root)
+		return max
+	}
+	return 0
+}
+
+func calcHeight(root *TreeNode)int {
+	if root != nil {
+		left := calcHeight(root.Left)
+		right := calcHeight(root.Right)
+		if left+right>max{
+			max = left+right
+		}
+		return Max(left,right)+1
+	}
+	return 0
+}
+
+func Max(a,b int)int{
+	if a >b {
+		return a
+	}
+	return b
 }
--- tree/tree_test.go
+++ tree/tree_test.go
@@ -2,10 +2,11 @@ package tree
 
 import (
 	"fmt"
+	"reflect"
 	"testing"
 )
 
-func TestLevelOrder(t *testing.T){
+func TestLevelOrder(t *testing.T) {
 	tree := &TreeNode{
 		Val: 3,
 		Left: &TreeNode{
@@ -23,7 +24,27 @@ func TestLevelOrder(t *testing.T){
 	}
 
 	ret := levelOrder(tree)
-	for _,row := range ret {
+	for _, row := range ret {
 		fmt.Println(row)
+	}
+}
+
+func Test_levelOrder(t *testing.T) {
+	type args struct {
+		root *TreeNode
+	}
+	tests := []struct {
+		name string
+		args args
+		want [][]int
+	}{
+		// TODO: Add test cases.
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := levelOrder(tt.args.root); !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("levelOrder() = %v, want %v", got, tt.want)
+			}
+		})
 	}
 }
