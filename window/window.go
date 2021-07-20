72242473a9a2d3b440d41a6b067e7db8966b7380
周全 <946778530@qq.com>
2021-07-21 00:35:07 +0800

    [add] sub array

--- window/window.go
+++ window/window.go
@@ -1,5 +1,7 @@
 package window
 
+import "fmt"
+
 // 无重复字符最长子串
 func lengthOfLongestSubstring(s string) int {
 	size := len(s)
@@ -19,4 +21,71 @@ func lengthOfLongestSubstring(s string) int {
 		end++
 	}
 	return max
+}
+
+// 最短无序连续子数组
+
+func findUnsortedSubarray(nums []int) int {
+	if nums == nil || len(nums) < 2 {
+		return 0
+	}
+	left := 0
+	right := 0
+	max := nums[0]
+	for i := 0; i < len(nums); i++ {
+		if nums[i] < max {
+			left = i
+			break
+		}
+		max = nums[i]
+	}
+	min := nums[len(nums)-1]
+	for i := len(nums) - 1; i >= 0; i-- {
+		if nums[i] > min {
+			right = i
+			break
+		}
+		min = nums[i]
+	}
+	fmt.Println(left, right)
+	if left == right {
+		return 0
+	}
+	return right - left + 3
+}
+
+func findUnsortedSubarray2(nums []int) int {
+	if nums == nil || len(nums) < 2 {
+		return 0
+	}
+	size := len(nums)
+	high, low := 0, size-1
+	max, min := nums[0], nums[size-1]
+	for i := 1; i < size; i++ {
+		max = Max(max, nums[i])
+		min = Min(min, nums[size-1-i])
+		if nums[i] < max {
+			high = i
+		}
+		if nums[size-1-i] > min {
+			low = size - 1 - i
+		}
+	}
+	if high > low {
+		return high - low + 1
+	}
+	return 0
+}
+
+func Max(a, b int) int {
+	if a > b {
+		return a
+	}
+	return b
+}
+func Min(a, b int) int {
+	if a > b {
+		return b
+	}
+	return a
 }
--- window/window_test.go
+++ window/window_test.go
@@ -8,3 +8,10 @@ import (
 func Test_lengthOfLongestSubstring(t *testing.T) {
 	fmt.Println(lengthOfLongestSubstring("abcabcbb"))
 }
+
+func TestFindUnsortedSubarray(t *testing.T) {
+	fmt.Println(findUnsortedSubarray([]int{2,6,4,8,10,9,15}))
+	fmt.Println(findUnsortedSubarray([]int{1,2,3,4}))
+	fmt.Println(findUnsortedSubarray([]int{2,1}))
+	fmt.Println(findUnsortedSubarray([]int{5,4,3,2,1}))
+}
