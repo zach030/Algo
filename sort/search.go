f9912c2aea925203b9997bea73fdbc44ac77a98d
周全 <946778530@qq.com>
2021-09-27 00:39:38 +0800

    feat add quicksort

--- sort/search.go
+++ sort/search.go
@@ -1,5 +1,7 @@
 package sort
 
+import "fmt"
+
 func binarySearch(nums []int, target int) int {
 	low, high := 0, len(nums)-1
 	for low <= high {
@@ -33,4 +35,32 @@ func bsSearchInternal(nums []int, low, high, value int) int {
 		return bsSearchInternal(nums, low, mid+1, value)
 	}
 	return -1
+}
+
+func findKMax(nums []int, left, right, k int) {
+	pos := partition(nums, left, right)
+	fmt.Println("pos is:", pos, ", k is:", k, " ,arr is:", nums)
+	if pos == k-1 {
+		fmt.Println(nums[pos])
+	} else if pos > k-1 {
+		findKMax(nums, left, pos-1, k)
+	} else {
+		findKMax(nums, pos+1, right, k-pos)
+	}
+}
+
+func partition(nums []int, left, right int) int {
+	tmp := nums[left]
+	for left < right {
+		for left < right && tmp >= nums[right] {
+			right--
+		}
+		nums[left] = nums[right]
+		for left < right && tmp <= nums[left] {
+			left++
+		}
+		nums[right] = nums[left]
+	}
+	nums[left] = tmp
+	return right
 }
--- sort/search_test.go
+++ sort/search_test.go
@@ -12,3 +12,42 @@ func Test_binarySearch(t *testing.T) {
 
 	fmt.Println(bsSearchInternal(arr, 0, len(arr)-1, target))
 }
+
+func TestFindKMax(t *testing.T) {
+	nums := []int{3, 2, 3, 1, 2, 4, 5, 5, 6}
+	findKMax(nums, 0, len(nums)-1, 4)
+	findKMax2(nums, 0, len(nums)-1, 4)
+
+	arr := []int{5, 3, 5, 4}
+	fmt.Println(partition(arr, 0, len(arr)-1))
+}
+
+func findKthLargest(nums []int, k int) int {
+	return findKMax2(nums,0,len(nums)-1,k)
+}
+
+func findKMax2(nums []int,left,right,k int)int{
+	pos := partition2(nums,left,right)
+	if pos == k{
+		return nums[pos]
+	}else if pos>k{
+		return partition2(nums,pos+1,right)
+	}
+	return partition2(nums,left,pos-1)
+}
+
+func partition2(nums []int,left,right int)int{
+	tmp := nums[left]
+	for left<right{
+		for left<right && nums[left]<=tmp{
+			left++
+		}
+		nums[left]=nums[right]
+		for left<right && nums[right]>=tmp{
+			right--
+		}
+		nums[right]=nums[left]
+	}
+	nums[left]=tmp
+	return right
+}
