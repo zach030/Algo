bbecfeca14bd58bc1a6e27a2b76ac575a6c509a5
zach <zachchou016@gmail.com>
2021-12-22 10:07:28 +0800

    add mid dp
    Signed-off-by: zach <zachchou016@gmail.com>

--- daily/dp.go
+++ daily/dp.go
@@ -69,3 +69,69 @@ func maxProfit(prices []int) int {
 	}
 	return profit
 }
+
+// 连续子数组最大和,f(n)表示以num[n]结尾的最大子数组和
+// f(n-1)<=0,f(n)=nums[n]
+// f(n-1)>0,f(n)=f(n-1)+nums[n]
+func maxSubArray(nums []int) int {
+	if len(nums) == 0 {
+		return 0
+	}
+	var last = nums[0]
+	var curr = 0
+	var maxSum = last
+	for i := 1; i < len(nums); i++ {
+		if last <= 0 {
+			curr = nums[i]
+		} else {
+			curr = last + nums[i]
+		}
+		if curr > maxSum {
+			maxSum = curr
+		}
+		last = curr
+	}
+	return maxSum
+}
+
+// 礼物的最大价值
+//输入:
+//[
+//  [1,3,1],
+//  [1,5,1],
+//  [4,2,1]
+//]
+//输出: 12
+//解释: 路径 1→3→5→2→1 可以拿到最多价值的礼物
+
+// dp[i][j]表示从[0,0]到[i,j]拿到的最多价值礼物
+// 最后一步到达dp[i][j]有两条路:[i][j-1]--->[i][j], [i-1][j]--->[i][j]，取两者的最大值即可
+// dp[i][j]=max(dp[i-1][j]+grid[i][j],dp[i][j-1]+grid[i][j])
+func maxValue(grid [][]int) int {
+	if len(grid) == 0 {
+		return 0
+	}
+	m, n := len(grid), len(grid[0])
+	dp := make([][]int, len(grid))
+	for i := 0; i < len(dp); i++ {
+		dp[i] = make([]int, len(grid[0]))
+	}
+	dp[0][0] = grid[0][0]
+	for i := 0; i < m; i++ {
+		for j := 0; j < n; j++ {
+			if i == 0 && j == 0 {
+				continue
+			}
+			if i == 0 {
+				dp[i][j] = dp[i][j-1] + grid[i][j]
+				continue
+			}
+			if j == 0 {
+				dp[i][j] = dp[i-1][j] + grid[i][j]
+				continue
+			}
+			dp[i][j] = max(dp[i-1][j]+grid[i][j], dp[i][j-1]+grid[i][j])
+		}
+	}
+	return dp[m-1][n-1]
+}
--- daily/dp_test.go
+++ daily/dp_test.go
@@ -12,3 +12,17 @@ func Test_fib(t *testing.T) {
 func TestMaxProfit(t *testing.T) {
 	fmt.Println(maxProfit([]int{7, 1, 5, 3, 6, 4}))
 }
+
+func TestMaxSumSubArr(t *testing.T) {
+	a1 := []int{-2, 1, -3, 4, -1, 2, 1, -5, 4}
+	fmt.Println(maxSubArray(a1))
+}
+
+func TestMaxValue(t *testing.T) {
+	g := [][]int{
+		{1, 3, 1},
+		{1, 5, 1},
+		{4, 2, 1},
+	}
+	fmt.Println(maxValue(g))
+}
