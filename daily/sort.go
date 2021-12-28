45bf735561d29f3e89608f56413f437ac3e1b90a
zach <zachchou016@gmail.com>
2021-12-28 10:04:34 +0800

    add basic sort
    Signed-off-by: zach <zachchou016@gmail.com>

---
+++ daily/sort.go
@@ -0,0 +1,38 @@
+package daily
+
+import (
+	"sort"
+	"strconv"
+	"strings"
+)
+
+type strs []string
+
+func (s strs) Len() int {
+	return len(s)
+}
+
+func (s strs) Less(i, j int) bool {
+	return s[i]+s[j] < s[j]+s[i]
+}
+
+func (s strs) Swap(i, j int) {
+	s[i], s[j] = s[j], s[i]
+}
+
+// 把数组排成最小的数
+// 先比第一位，再比第二位，小的放前面
+func minNumber(nums []int) string {
+	arr := make(strs, len(nums))
+	for i, num := range nums {
+		arr[i] = strconv.Itoa(num)
+	}
+	sort.Sort(arr)
+	return strings.Join(arr, "")
+}
+
+// 判断是否是连续数组
+func isStraight(nums []int) bool {
+	//todo 12.28
+	return true
+}
---
+++ daily/sort_test.go
@@ -0,0 +1,11 @@
+package daily
+
+import (
+	"fmt"
+	"testing"
+)
+
+func Test_minNumber(t *testing.T) {
+	arr := []int{3, 30, 34, 5, 9}
+	fmt.Println(minNumber(arr))
+}
